a26313e491d57518c75c18a6a650cb278fa6d4e5
Joao Pedro Ronzani dos Santos <joao.ronzani@betha.com.br>
2021-05-06 09:08:45 -0300

    categorias get

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 export class HomePage implements OnInit {
   public home: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, public navCtrl: NavController, public menu: MenuController) { }
 
+  ionViewWillEnter() {
+    this.menu.enable(false);
+  }
+  ionViewDidLeave() {
+    this.menu.enable(true);
+  }
   ngOnInit() {
     this.home = this.activatedRoute.snapshot.paramMap.get('id');
   }
-
+  login(){
+    this.navCtrl.navigateForward("/categorias");
+  }
 }
